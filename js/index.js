fcebd37596d4ee4e4be03077cf76a6b4995c3837
Sincere <dukaiqiang@xindong.com>
2017-11-27 18:10:05 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
 (function($) {
     var sdkList = {
         "iOS": [{
+            "time": "2017-11-27",
+            "size": "5.7M",
+            "version": "3.2.1",
+            "demo": "./xdsdk_docs/iOS/V3.2.1/XDSDK_Demo.zip",
+            "sdk": "./xdsdk_docs/iOS/V3.2.1/XDSDK-iOS-3.2.1-20171127.zip",
+            "doc": "./xdsdk_docs/iOS/V3.2.1/xd_doc_ios.html",
+            "changeLog": "./xdsdk_docs/iOS/CHANGELOG.html"
+        },{
             "time": "2017-10-18",
             "size": "5.7M",
             "version": "3.2.0",
